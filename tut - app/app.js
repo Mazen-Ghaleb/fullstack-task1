cda30af345a0ed5d852322b2d7f0c8f7860414a4
Mazen9 <mmzn971@gmail.com>
2021-08-31 00:03:04 +0200

    Finished Tutorial#8 and cleared previous not needed comments and html files

--- tut - app/app.js
+++ tut - app/app.js
@@ -1,19 +1,31 @@
 const express = require('express');
-
+const morgan = require('morgan');
 // express app
 const app = express();
 
 // register view engine
 app.set('view engine', 'ejs');
-//app.set('views', '//location');
 
 // listen to requests
 app.listen(3000);
 
+// middleware & static files
+app.use(express.static('public'));
+
+app.use(morgan('dev'));
+
+// helmet --> used for security
+// morgan --> is better than code below
+// app.use((req, res, next) => {
+//   console.log('new request made:');
+//   console.log('host: ', req.hostname);
+//   console.log('path: ', req.path);
+//   console.log('method: ', req.method);
+//   next();
+// });
+
 // home page
 app.get('/', (req, res) => {
-  //res.send('<p>home page</p>');
-  //res.sendFile('./views/index.html', { root: __dirname });
   const blogs = [
     { title: 'How to Play Mario', snippet: 'W,A,S,D to move around' },
     { title: 'How to Download Mario', snippet: 'You have to have win32' },
@@ -22,25 +34,22 @@ app.get('/', (req, res) => {
   res.render('index', { heading: 'All Blogs', blogs });
 });
 
+// app.use((req, res, next) => {
+//   console.log('test2');
+//   next();
+// });
+
 // about page
 app.get('/about', (req, res) => {
-  //res.send('<p>about page</p>');
-  //res.sendFile('./views/about.html', { root: __dirname });
   res.render('about', { heading: 'About Page' });
 });
 
 // create blog page
 app.get('/blogs/create', (req, res) => {
-  res.render('create', { heading: 'Create Blog Page' });
+  res.render('create', { heading: 'Create a new Blog' });
 });
-
-// redirects
-// app.get('/about-us', (req, res) => {
-//   res.redirect('/about');
-// });
 
 // 404 page ( if it does not match the requests above )
 app.use((req, res) => {
-  //res.status(404).sendFile('./views/404.html', { root: __dirname });
   res.status(404).render('404', { heading: '404 - ERROR PAGE' });
 });
