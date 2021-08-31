4c5670594c4af136425db43d43143cc10ae9eb3a
Mazen9 <mmzn971@gmail.com>
2021-08-31 21:11:57 +0200

    Getting rid of duplicate files cause of Heroku integration

--- tut - app/routes/BooksRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const BooksController = require('../controllers/BooksController');
-const router = express.Router();
-
-router.get('/', BooksController.Books_info);
-
-module.exports = router;
